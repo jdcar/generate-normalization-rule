15665371d8f811b6767ba06bb1da6776759815cd
jdcar <jdcarlstone@gmail.com>
2021-06-09 15:36:11 -0500

    Fixed subfield issue in generated file

--- utils/generateNormRule.js
+++ utils/generateNormRule.js
@@ -13,12 +13,15 @@ function generateNormRule(data) {
 // Generate normalization rule content
 
   let normRule = 
-`rule "Delete FAST headings"
-priority 2
+`rule "Add LCSH to local tags"
+priority 4
 when 
-(true)
-then
-removeField "${lcshTag}" if (exists "${lcshTag}.{-,7}.${subfield}.${lcshHeading}*")
+(exists "${lcshTag}.${subfield}.${lcshHeading}*")
+then 
+addSubField "${lcshTag}.2.lcsh" if (exists "${lcshTag}.{-,0}.${subfield}.${lcshHeading}*")
+copyField "${lcshTag}" to "${localTag}" if (exists "${lcshTag}.${subfield}.${lcshHeading}*")
+removeSubField "${lcshTag}.2" if (exists "${lcshTag}.{-,0}")
+
 end
 
 rule "Flip from LCSH to NULA"
@@ -26,31 +29,24 @@ priority 3
 when
 (true)
 then
-replaceContents "${lcshTag}.${subfield}.${lcshHeading}\\\\\\\\." with "${localHeading}." if(exists "${lcshTag}.{*,0}.${subfield}.${lcshHeading}\\\\\\\\.")
+replaceContents "${lcshTag}.${subfield}.${lcshHeading}\\\\." with "${localHeading}." if(exists "${lcshTag}.{*,0}.${subfield}.${lcshHeading}\\\\.")
 replaceContents "${lcshTag}.${subfield}.${lcshHeading}" with "${localHeading}" if(exists "${lcshTag}.{*,0}.${subfield}.${lcshHeading}")
-changeSecondIndicator "${lcshTag}" to "7" if (exists "${lcshTag}.${subfield}.${localHeading}\\\\\\\\.")
+changeSecondIndicator "${lcshTag}" to "7" if (exists "${lcshTag}.${subfield}.${localHeading}\\\\.")
 changeSecondIndicator "${lcshTag}" to "7" if (exists "${lcshTag}.${subfield}.${localHeading}")
-removeSubField "${lcshTag}.2" if (exists "${lcshTag}.{-,7}")
-addSubField "${lcshTag}.2.${vocabCode}"  if (exists "${lcshTag}.{-,7}.${subfield}.${localHeading}\\\\\\\\.")
+removeSubField "${lcshTag}.2.fast" if (exists "${lcshTag}.{-,7}")
+addSubField "${lcshTag}.2.${vocabCode}"  if (exists "${lcshTag}.{-,7}.${subfield}.${localHeading}\\\\.")
 addSubField "${lcshTag}.2.${vocabCode}"  if (exists "${lcshTag}.{-,7}.${subfield}.${localHeading}")
+
 end
 
-rule "Add LCSH to local tags"
-priority 4
-when 
-(exists "${lcshTag}.${subfield}.${lcshHeading}*")
-then 
-copyField "${lcshTag}" to "${localTag}" if (exists "${lcshTag}.${subfield}.${lcshHeading}*")
-end
-
-rule "add $2 lcsh to lcsh headings"
-priority 5
+rule "Delete FAST headings"
+priority 2
 when 
 (true)
 then
-addSubField "${lcshTag}.2.lcsh"  if (exists "${lcshTag}.{-,0}.${subfield}.${lcshHeading}\\\\\\\\.")
-addSubField "${lcshTag}.2.lcsh"  if (exists "${lcshTag}.{-,0}.${subfield}.${lcshHeading}")
-end`
+removeField "${lcshTag}" if (exists "${lcshTag}.{-,7}.${subfield}.${lcshHeading}*")
+end
+`
 
 // Create a readme file
   fs.writeFile('generated-normalization-rule.txt', normRule, (err) => {
